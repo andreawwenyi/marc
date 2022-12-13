8d1f343d4e2b64a827d994c831d7a09e0865997d
andrea-w-wang <ayw255@nyu.edu>
2022-12-13 12:55:07 +0800

    fix: pretrained model give predictions

--- en_prediction.py
+++ en_prediction.py
@@ -1,56 +1,33 @@
-import json
 import pickle as pk
 
 import torch
-import transformers
-from sklearn.metrics import classification_report
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 checkpoint = "./results/checkpoint-37500"
 model_name = 'distilbert-base-uncased'
 data_dir = "./data/"
 device_name = 'cuda'
+num_labels = 2
 data = {}
 
-
-class SCDataset(torch.utils.data.Dataset):
-    def __init__(self, encodings, labels):
-        self.encodings = encodings
-        self.labels = labels
-
-    def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
-        item['labels'] = torch.tensor(self.labels[idx])
-        return item
-
-    def __len__(self):
-        return len(self.labels)
-
-
 print("init model...")
-model = transformers.AutoModel.from_pretrained(checkpoint).to(device_name)
-tokenizer = transformers.AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=num_labels).to(device_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-for dataset_name in ['test', 'dev']:
-    print(dataset_name)
-    raw_data = [json.loads(line) for line in open(data_dir + f'dataset_en_{dataset_name}.json', 'r', encoding='utf-8')]
-    data[dataset_name] = {
-        "text": [r['review_body'] for r in raw_data],
-        "binary_labels": [0 if int(r['stars']) <= 3 else 1 for r in raw_data],
-        "original_labels": [r['stars'] for r in raw_data],
-    }
-    print("generating encoding...")
-    data[dataset_name]['encoding'] = [tokenizer(t, truncation=True, padding=True, return_tensors='pt') for t in
-                                      data[dataset_name]['text']]
-    # print("generating dataset structure...")
-    # data[dataset_name]['torch_dataset'] = SCDataset(data[dataset_name]['encoding'], data[dataset_name]['binary_labels'])
-    pk.dump(data[dataset_name], open(f"distilbert_dataset_en_{dataset_name}.pk", "wb"))
 
 predictions = dict()
-for dataset_name in ['test', 'dev']:
+for segment_name in ['test', 'dev']:
+    print(segment_name)
+    data = pk.load(open(f"distilbert_dataset_en_{segment_name}.pk", "rb"))
+
+    print("encoding...")
+    inputs = data['text']
+    encoding = tokenizer(inputs, truncation=True, padding=True, return_tensors='pt')
     print("making predictions...")
-    predictions[dataset_name] = list()
-    for enc in data[dataset_name]['encoding']:
-        predictions[dataset_name].append(model(**enc))
-pk.dump(predictions, open(f"distilbert_predictions_en.pk", "wb"))
-    # actual_predicted_labels = predicted_labels.predictions.argmax(-1)
-    # print(classification_report(predicted_labels.label_ids.flatten(), actual_predicted_labels.flatten()))
+    model.eval()  # set to evaluation mode
+    with torch.no_grad():
+        output = model(**encoding)
+        _, pred = torch.max(output[0], dim=1)
+    predictions[segment_name] = pred
+
+pk.dump(predictions, open(f"distilbert_predictions_en_binary.pk", "wb"))
